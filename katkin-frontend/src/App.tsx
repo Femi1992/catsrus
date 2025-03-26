c882dc6193f52e4fd3b275e82a16897d8600815a
Femi Dokun <30286598+Femi1992@users.noreply.github.com>
2025-03-26 21:29:38 +0000

    added error handling for UI

--- katkin-frontend/src/App.tsx
+++ katkin-frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NextDeliveryPage from './displayNextDelivery';
+import NotFoundPage from './notFoundPage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/comms/your-next-delivery/:id" element={<NextDeliveryPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
